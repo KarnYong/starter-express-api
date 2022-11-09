adde14807a22c5549c3c7df6d58f8db4a64e9517
Karn Yongsiriwit <KarnYong@gmail.com>
2022-11-09 22:20:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'us-east.connect.psdb.cloud',
+  user: 'y5glnjpyqjrl6yv72caz',
+  password: 'pscale_pw_kvayJuXwZeWjkrn89rghoTQnppiAHEUUtyW0Z3KZ2fF',
+  database: 'melivecode',
+  ssl: {}
+});
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get('/attractions', function (req, res, next) {
+  connection.query(
+    'SELECT * FROM attractions',
+    function(err, results, fields) {
+      res.json(results);
+    }
+  );
+})
+
 app.listen(process.env.PORT || 3000)
